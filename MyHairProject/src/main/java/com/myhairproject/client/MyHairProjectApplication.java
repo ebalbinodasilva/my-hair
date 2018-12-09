823af6ac5cfd702f4a42bbe22c3f9e8a53955dda
BalbinoSilva <everton16.b@gmail.com>
2018-12-09 11:45:20 -0200

    first deploy

---
+++ MyHairProject/src/main/java/com/myhairproject/client/MyHairProjectApplication.java
@@ -0,0 +1,12 @@
+package com.myhairproject.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHairProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHairProjectApplication.class, args);
+	}
+}
